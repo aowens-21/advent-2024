6107e3decb2bbfc08944c5b95d4ebd3f79570837
Alex Owens <aowens5189@gmail.com>
2024-12-06 00:38:38 -0500

    Day 5 part 2

--- day5.go
+++ day5.go
@@ -76,5 +76,65 @@ func getMiddle(nums []int) int {
 }
 
 func Day5Part2(lines []string) int {
-	return -1
+	ruleMap := make(map[int][]int)
+
+	parsingRules := true
+	var unsafeLines [][]int
+
+	for _, l := range lines {
+		if len(l) < 2 {
+			parsingRules = false
+		} else if parsingRules {
+			orderingPair := strings.Split(l, "|")
+			key, _ := strconv.Atoi(orderingPair[0])
+			afterNum, _ := strconv.Atoi(orderingPair[1])
+
+			vals, ok := ruleMap[key]
+
+			if ok {
+				ruleMap[key] = append(vals, afterNum)
+			} else {
+				ruleMap[key] = []int{afterNum}
+			}
+		} else {
+			updateStrs := strings.Split(l, ",")
+			var updateNums []int
+
+			for _, str := range updateStrs {
+				result, _ := strconv.Atoi(str)
+				updateNums = append(updateNums, result)
+			}
+
+			if !isPrintSafe(updateNums, ruleMap) {
+				unsafeLines = append(unsafeLines, updateNums)
+			}
+		}
+	}
+
+	sum := 0
+	for _, l := range unsafeLines {
+		for _, c := range l {
+			mustGoBeforeNums, _ := ruleMap[c]
+			numsInBoth := intersect(l, mustGoBeforeNums)
+
+			if len(numsInBoth) == len(l)/2 {
+				sum += c
+				break
+			}
+		}
+	}
+
+	return sum
+}
+
+func intersect(arr1 []int, arr2 []int) []int {
+	var l []int
+
+	for _, item := range arr1 {
+		if slices.Contains(arr2, item) {
+			l = append(l, item)
+		}
+	}
+
+	return l
 }
