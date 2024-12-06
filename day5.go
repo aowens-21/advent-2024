d9a79005c97da2347661f9194314303582204c07
Alex Owens <aowens5189@gmail.com>
2024-12-05 21:22:11 -0500

    Day 5 part 1

--- day5.go
+++ day5.go
@@ -1,13 +1,80 @@
 package main
 
+import (
+	"slices"
+	"strconv"
+	"strings"
+)
+
 func Day5(lines []string) (int, int) {
-	return Day5Part1(), Day5Part2()
+	return Day5Part1(lines), Day5Part2(lines)
 }
 
-func Day5Part1() int {
-	return -1
+func Day5Part1(lines []string) int {
+	ruleMap := make(map[int][]int)
+
+	parsingRules := true
+	var safeLines [][]int
+
+	for _, l := range lines {
+		if len(l) < 2 {
+			parsingRules = false
+		} else if parsingRules {
+			orderingPair := strings.Split(l, "|")
+			key, _ := strconv.Atoi(orderingPair[0])
+			afterNum, _ := strconv.Atoi(orderingPair[1])
+
+			vals, ok := ruleMap[key]
+
+			if ok {
+				ruleMap[key] = append(vals, afterNum)
+			} else {
+				ruleMap[key] = []int{afterNum}
+			}
+		} else {
+			updateStrs := strings.Split(l, ",")
+			var updateNums []int
+
+			for _, str := range updateStrs {
+				result, _ := strconv.Atoi(str)
+				updateNums = append(updateNums, result)
+			}
+
+			if isPrintSafe(updateNums, ruleMap) {
+				safeLines = append(safeLines, updateNums)
+			}
+		}
+	}
+
+	sum := 0
+
+	for _, lines := range safeLines {
+		sum += getMiddle(lines)
+	}
+
+	return sum
 }
 
-func Day5Part2() int {
+func isPrintSafe(nums []int, ruleMap map[int][]int) bool {
+	for i, num := range nums {
+		numsAfter := ruleMap[num]
+
+		for j := i + 1; j < len(nums); j++ {
+			if !slices.Contains(numsAfter, nums[j]) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func getMiddle(nums []int) int {
+	middle := len(nums) / 2
+
+	return nums[middle]
+}
+
+func Day5Part2(lines []string) int {
 	return -1
 }
